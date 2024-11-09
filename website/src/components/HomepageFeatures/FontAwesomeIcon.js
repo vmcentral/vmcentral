e9005b3355ea562f52116b788a704ad19a580b24
Debsify Satisfying <debsify@outlook.com>
2024-11-09 12:30:01 +0000

    hapus

--- website/src/components/HomepageFeatures/FontAwesomeIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
-
-function MyFontAwesomeIcon({ icon }) {
-  return <FontAwesomeIcon icon={icon} />;
-}
-
-export default MyFontAwesomeIcon;
